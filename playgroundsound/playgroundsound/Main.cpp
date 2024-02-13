fdc44f36845115bafd2fd48ba2baf481037e9d07
jojjejoel <71003153+jojjejoel@users.noreply.github.com>
2024-02-13 20:05:26 +0100

    Wwise initializing properly, not posting event yet

--- playgroundsound/playgroundsound/Main.cpp
+++ playgroundsound/playgroundsound/Main.cpp
@@ -1,20 +1,48 @@
-#include "RaylibWrapper.h"
+//#include "RaylibWrapper.h"
 #include "WwiseWrapper.h"
 #include <iostream>
 
 int main(void)
 {
-    // Wwise Init
-    //WwiseWrapper wwiseWrapper;
+     //Wwise Init
+    WwiseWrapper wwiseWrapper;
 
-    //if (wwiseWrapper.Init())
-    //{
-    //    //std::cout << "WwiseWrapper initialized" << std::endl;
-    //}
-    
-    
-    RaylibWrapper raylibWrapper;
+    if (wwiseWrapper.Init())
+    {
+        std::cout << "WwiseWrapper initialized" << std::endl;
+    }
+    else
+    {
+        std::cout << "ERROR" << std::endl;
+    }
 
-    raylibWrapper.Run();
+    AkGameObjectID gameObjectIDTest = 1;
+
+    if (AK::SoundEngine::RegisterGameObj(gameObjectIDTest, "Test") == AK_Success)
+    {
+        std::cout << "GameObject registered" << std::endl;
+    }
+    
+    if (AK::SoundEngine::PostEvent(AK::EVENTS::PLAY_TEST, gameObjectIDTest) == AK_Success)
+    {
+        std::cout << "Posted event!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Post event FAILED" << std::endl;
+    }
+
+    
+    int a = 0;
+
+    AK::SoundEngine::RenderAudio();
+    
+
+    std::cin >> a;
+
+    std::cout << "Program terminated" << std::endl;
+    //RaylibWrapper raylibWrapper;
+
+    //raylibWrapper.Run();
     return 0;
 }
--- playgroundsound/playgroundsound/WwiseWrapper.h
+++ playgroundsound/playgroundsound/WwiseWrapper.h
@@ -1,19 +1,29 @@
 #pragma once
 
-// Memory management
+//// Memory management
 #include <AK/SoundEngine/Common/AkMemoryMgr.h>
 #include <AK/SoundEngine/Common/AkModule.h>
-
-// Streaming
+//
+//// Streaming
 #include <AK/SoundEngine/Common/IAkStreamMgr.h>
 #include <AK/Tools/Common/AkPlatformFuncs.h>
 #include <AkFilePackageLowLevelIOBlocking.h>
 
-// File packaging
+
+//// File packaging
+#include "AkFilePackage.h"
+#include "AkFilePackageLUT.h"
+
+
+#include "Wwise_IDs.h"
+
+#define BANKNAME_INIT L"Init.bnk"
+#define BANKNAME_MAIN L"Main.bnk"
 
 class WwiseWrapper
 {
 
+	CAkFilePackageLowLevelIOBlocking g_lowLevelIO;
 	public: bool Init();
 
 };
